22a2d058c3a477a50cc4965240654ea1ee2f519d
LambdazZ <lambdaz.Michael@gmail.com>
2023-11-11 13:20:32 +0800

    feat: Add dynamic modification of pie chart based on current table display

--- SDN/detail.js
+++ SDN/detail.js
@@ -1,29 +1,42 @@
-function addRow() 
-{
+document.addEventListener("DOMContentLoaded", function() {
+  var tableContainer = document.getElementById("table-container");
   var table = document.getElementById("data-table");
-  var tbody = table.getElementsByTagName("tbody")[0];
-  var newRow = tbody.insertRow();
-  var cell1 = newRow.insertCell();
-  cell1.innerHTML = "新行，列1";
-  var cell2 = newRow.insertCell();
-  cell2.innerHTML = "新行，列2";
-  var cell3 = newRow.insertCell();
-  cell3.innerHTML = "新行，列3";
-}
 
-function clean()
-{
-  console.log(1111)
-  var table = document.getElementById("data-table");
-  var oldTbody = table.getElementsByTagName("tbody")[0];
-  var newTbody = document.createElement("tbody");
-  table.replaceChild(newTbody, oldTbody);
-}
+  function addRow() 
+  {
+    var tbody = table.getElementsByTagName("tbody")[0];
+    for(var i = 0; i < 20; ++i)
+    {
+      var newRow = tbody.insertRow();
+      var cell1 = newRow.insertCell();
+      cell1.innerHTML = i + "行，列1";
+      var cell2 = newRow.insertCell();
+      cell2.innerHTML = i + "行，列2";
+      var cell3 = newRow.insertCell();
+      cell3.innerHTML = i + "行，列3";
+      var cell4 = newRow.insertCell();
+      cell4.innerHTML = i + "行，列4";
+      var cell5 = newRow.insertCell();
+      cell5.innerHTML = i + "行，列5";
+      var cell6 = newRow.insertCell();
+      cell6.innerHTML = Math.random() * 2023;
+    }
+  }
 
-function update()
-{
-  clean();
-  addRow();
-}
+  function clean()
+  {
+    var table = document.getElementById("data-table");
+    var oldTbody = table.getElementsByTagName("tbody")[0];
+    var newTbody = document.createElement("tbody");
+    table.replaceChild(newTbody, oldTbody);
+    
+  }
 
-setInterval(update, 5000);
+  function update()
+  {
+    clean();
+    addRow();
+  }
+
+  setInterval(update, 5000);
+});
--- SDN/flow.js
+++ SDN/flow.js
@@ -1,75 +1,112 @@
-var flowChart = echarts.init(document.getElementById('nightingale'));
+document.addEventListener("DOMContentLoaded", function() {
+  var tableContainer = document.getElementById("table-container");
+  var table = document.getElementById("data-table");
+  var flowChart = echarts.init(document.getElementById('nightingale'));
 
-flowOption = {
-    legend: {
-      top: 'bottom'
-    },
-    toolbox: {
-      show: true,
-      feature: {
-        mark: { show: true },
-        dataView: { show: true, readOnly: true },
-        restore: { show: false },
-        saveAsImage: { show: false }
+  flowOption = {
+      legend: {
+        top: 'bottom'
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: true },
+          restore: { show: false },
+          saveAsImage: { show: false }
+        }
+      },
+      series: [
+        {
+          name: '流量统计数据',
+          type: 'pie',
+          radius: [10, 80],
+          center: ['50%', '50%'],
+          roseType: 'area',
+          itemStyle: {
+            borderRadius: 6
+          },
+          data: [
+            { value: 40, name: '1' },
+            { value: 38, name: '2' },
+            { value: 32, name: '3' },
+            { value: 30, name: '4' },
+            { value: 28, name: '5' },
+            { value: 26, name: '6' },
+            { value: 22, name: '7' },
+            { value: 18, name: '8' }
+          ]
+        }
+      ]
+    };
+  
+  
+  flowChart.setOption(flowOption);  
+  
+  window.addEventListener('resize', function() 
+  {
+      flowChart.resize();
+  });
+  
+  function flowValueInit()
+  {
+      updateFlow();
+  }
+  
+  function init()
+  {
+      flowValueInit();
+  }
+  
+  function updateFlow()
+  {
+    var visibleRows = getVisibleRows(tableContainer);
+    var _data = [];
+    for (var i = 0; i < visibleRows.length; i++) {
+      var columns = Array.from(visibleRows[i].querySelectorAll("td"));
+      var sValue = columns[5] ? columns[5].textContent : 0;
+      var nameValue = "源IP: " + (columns[0] ? columns[0].textContent : "unknown");
+      var sObject = { value: sValue, name: nameValue };
+      
+      _data.push(sObject);
+    }
+    flowOption.series[0].data = _data;
+    console.log(flowOption.series[0].data);
+    flowChart.setOption(flowOption);
+  }
+
+  init();
+
+  function getVisibleRows(container) {
+    var table = container.querySelector("table");
+    var tbody = table.querySelector("tbody");
+    var rows = Array.from(tbody.querySelectorAll("tr"));
+  
+    var visibleRows = rows.filter(function (row) {
+      var rowRect = row.getBoundingClientRect();
+      var containerRect = container.getBoundingClientRect();
+      return (
+        rowRect.top >= containerRect.top &&
+        rowRect.bottom <= containerRect.bottom
+      );
+    });
+  
+    return visibleRows;
+  }
+  
+  tableContainer.addEventListener("scroll", function () 
+  {
+    updateFlow();
+  });
+
+  const observer = new MutationObserver(function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList' && mutation.target === table) {
+        updateFlow();
       }
-    },
-    series: [
-      {
-        name: '流量统计数据',
-        type: 'pie',
-        radius: [10, 100],
-        center: ['50%', '50%'],
-        roseType: 'area',
-        itemStyle: {
-          borderRadius: 6
-        },
-        data: [
-          { value: 40, name: '1' },
-          { value: 38, name: '2' },
-          { value: 32, name: '3' },
-          { value: 30, name: '4' },
-          { value: 28, name: '5' },
-          { value: 26, name: '6' },
-          { value: 22, name: '7' },
-          { value: 18, name: '8' }
-        ]
-      }
-    ]
-  };
-
-
-flowChart.setOption(flowOption);  
-
-window.addEventListener('resize', function() 
-{
-    flowChart.resize();
+    }
+  });
+  
+  const observerConfig = { childList: true, subtree: true };
+  observer.observe(table, observerConfig);
 });
-
-function flowValueInit()
-{
-    var size = flowOption.series[0].data.length;
-    for(var i = 0; i < size; ++i)
-        flowOption.series[0].data[i].value = Math.random() * 2023;
-    flowChart.setOption(flowOption);
-}
-
-function init()
-{
-    flowValueInit();
-}
-
-function updateFlowValue()
-{
-    var size = flowOption.series[0].data.length;
-    for(var i = 0; i < size; ++i)
-        flowOption.series[0].data[i].value = Math.random() * 2023;
-    flowChart.setOption(flowOption);
-}
-
-function update()
-{
-  updateFlowValue();
-}
-
-init();
-setInterval(update, 5000);
