f82c3b63bd4ac5f864cc1515dc6a6c3a6bca9485
LambdazZ <lambdaz.Michael@gmail.com>
2023-11-13 15:19:26 +0800

    feat: Add functionality to update Statistics information

--- 102101238/statistics/src/main/java/com/bitasync/statistics/controller/StatisticsController.java
+++ 102101238/statistics/src/main/java/com/bitasync/statistics/controller/StatisticsController.java
@@ -2,14 +2,9 @@ package com.bitasync.statistics.controller;
 
 import com.bitasync.statistics.model.Statistics;
 import com.bitasync.statistics.service.StatisticsService;
-import lombok.Data;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Lambda
@@ -39,5 +34,32 @@ public class StatisticsController
     Statistics getStatisticsByToken(@RequestBody Statistics statistics)
     {
         return statisticsService.getStatisticsByToken(statistics.getToken()).get();
+    }
+
+    @PostMapping(value = "/statistics/update")
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
+    void updateStatistics(@RequestBody Statistics statistics)
+    {
+        if(!statisticsService.getStatisticsByToken(statistics.getToken()).isPresent())
+            insert(statistics.getToken());
+
+        statistics.setEscapes(statistics.getEscapes() == null ? 0: statistics.getEscapes());
+        statistics.setTimeouts(statistics.getTimeouts() == null ? 0: statistics.getTimeouts());
+        statistics.setTotalOrders(statistics.getTotalOrders() == null ? 0: statistics.getTotalOrders());
+        statistics.setPositiveReviews(statistics.getPositiveReviews() == null ? 0: statistics.getPositiveReviews());
+        statistics.setNegativeReviews(statistics.getNegativeReviews() == null ? 0: statistics.getNegativeReviews());
+        statisticsService.updateStatistics(statistics);
+    }
+
+    void insert(String token)
+    {
+        Statistics statistics = new Statistics();
+        statistics.setToken(token);
+        statistics.setEscapes(0);
+        statistics.setTimeouts(0);
+        statistics.setTotalOrders(0);
+        statistics.setPositiveReviews(0);
+        statistics.setNegativeReviews(0);
+        statisticsService.insertStatistics(statistics);
     }
 }
